e90d753b1a22b8e7fddadd00c38d796a5b93e463
xmcc <xmcc@163.com>
2019-04-14 16:46:24 +0800

    第二次提交：test2

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -12,9 +12,9 @@ public class TestController {
 
     @RequestMapping("hello")
     public String test(){
-        log.debug("log debug ->{}","我是debug级别的日志1");
-        log.info(" log   info ->{}","我是info级别的日志1");
-        log.error("log error ->{}","我是error级别的日志1");
+        log.debug("log debug ->{}","我是debug级别的日志2");
+        log.info(" log   info ->{}","我是info级别的日志2");
+        log.error("log error ->{}","我是error级别的日志2");
 
         return "hello spring boot";
     }
